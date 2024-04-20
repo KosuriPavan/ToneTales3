6274d5d8c72c7b17f604f74af319ca4d63f9f239
SwathiPappu <106147230+KosuriPavan@users.noreply.github.com>
2024-04-20 11:19:37 +0530

    changes to vite to deply

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/homepage/HomePage.jsx";
 import AdminLogin from "./components/AdminLogin/LoginPage.jsx"
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<AdminLogin/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
